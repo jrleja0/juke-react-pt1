431266342ef4279f079f2a698c412578054e03da
Jasiu Leja <jleja09@gmail.com>
2017-05-16 11:30:07 -0400

    done workshop

--- browser/react/Main.js
+++ browser/react/Main.js
@@ -5,14 +5,33 @@ import Sidebar from './Sidebar';
 import Footer from './Footer';
 import Albums from './Albums';
 import fakeAlbums from './fakeAlbums';
+import axios from 'axios';
+
 
 
 class MainDiv extends Component {
   constructor(props){
     super(props);
     this.state = {
-      albums: fakeAlbums
+      albums: [   ],
+      selectedAlbum: {  }
     }
+  }
+
+  componentDidMount(){
+    const toJson = response => response.data;
+    // const log = console.log.bind(console);
+    const logError = console.error.bind(console);
+
+    axios.get('api/albums')
+      .then(toJson)
+      .then(albums => {
+        albums.map(album => {
+          album.imageUrl = `/api/albums/${album.id}/image`;
+          return album;
+        });
+        this.setState({ albums: albums })
+      ).catch(logError);
   }
 
   render(){
